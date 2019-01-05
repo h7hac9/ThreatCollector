aa6d07ec64ce68af392e48eeb1aa9bbf92008524
DaemonShao <daemonshao@gmail.com>
2019-01-05 14:44:06 +0800

    Added profile support for loop listening collection

--- ThreatCollector/spiders/hosts_file.py
+++ ThreatCollector/spiders/hosts_file.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+
+from ConfigParser import ConfigParser
+
 import scrapy
 
 from ThreatCollector.items import HostsFileItem
@@ -12,16 +15,22 @@ class HostsFileSpider(scrapy.Spider):
     def parse(self, response):
         # 使用RSS文件解析
         last_build = response.css("channel lastBuildDate::text").extract()[0]
-        for message_line in response.css("channel item"):
 
-            host_file_item = HostsFileItem()
-            host_file_item['host_name'] = message_line.css("title::text").extract()[0]
-            host_file_item['link'] = message_line.css("link::text").extract()[0]
-            description = message_line.css("description::text").extract()[0]
-            elements = description.split("<br>")
-            host_file_item["ip"] = elements[1].split(":")[1].strip(" ")
-            host_file_item["host_class"] = elements[2].split(":")[1].strip(" ")
-            host_file_item["add_time"] = message_line.css("pubDate::text").extract()[0]
-            host_file_item["last_build"] = last_build
+        conf = ConfigParser()
+        conf.read("crawl.ini")
+        last_last_build = conf.get(self.name, "last_build")
 
-            yield host_file_item
+        if last_last_build != last_build:
+            conf.set(self.name, "last_build", last_build)
+            for message_line in response.css("channel item"):
+                host_file_item = HostsFileItem()
+                host_file_item['host_name'] = message_line.css("title::text").extract()[0]
+                host_file_item['link'] = message_line.css("link::text").extract()[0]
+                description = message_line.css("description::text").extract()[0]
+                elements = description.split("<br>")
+                host_file_item["ip"] = elements[1].split(":")[1].strip(" ")
+                host_file_item["host_class"] = elements[2].split(":")[1].strip(" ")
+                host_file_item["add_time"] = message_line.css("pubDate::text").extract()[0]
+                host_file_item["last_build"] = last_build
+
+                yield host_file_item
