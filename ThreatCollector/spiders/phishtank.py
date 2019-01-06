36690b31ebb7fd3fde460cde147aaa22f4fe18c1
daemonshao@gmail.com <syq:415951323976X>
2019-01-06 12:14:07 +0800

    Add a loop crawl strategy

--- ThreatCollector/spiders/phishtank.py
+++ ThreatCollector/spiders/phishtank.py
@@ -2,26 +2,53 @@
 import scrapy
 from datetime import datetime
 import re
+from ConfigParser import ConfigParser
 
 from ThreatCollector.items import PhishtankItem
+
 
 class PhishtankSpider(scrapy.Spider):
     name = 'phishtank'
     allowed_domains = ['phishtank.com']
     start_urls = ['https://phishtank.com/phish_search.php?verified=u&active=y']
 
-    def parse(self, response):
-        short_urls = response.css("tr td a").xpath("@href").extract()
-        for short_url in short_urls:
-            if "phish_id" in short_url:
-                yield scrapy.Request(response.urljoin(short_url), callback=self.detailed_parse)
+    config = ConfigParser()
+    config.read("scrapy.cfg")
 
-        if len(response.css("td b a").xpath("@href").extract()) > 1:
-            next_right_short_url = response.css("td b a").xpath("@href").extract()[1]
-            yield scrapy.Request(response.urljoin(next_right_short_url), callback=self.parse)
-        else:
-            next_short_url = response.css("td b a").xpath("@href").extract_first()
-            yield scrapy.Request(response.urljoin(next_short_url), callback=self.parse)
+    def parse(self, response):
+        last_id = response.css("tr td a::text").extract_first()
+        short_urls = response.css("tr td a").xpath("@href").extract()
+
+        if self.config.get(self.name, "last_id") == "":
+
+            self.config.set(self.name, "last_id", last_id)
+            self.config.write(open("scrapy.cfg", "w+"))
+
+            for short_url in short_urls:
+                if "phish_id" in short_url:
+                    yield scrapy.Request(response.urljoin(short_url), callback=self.detailed_parse)
+
+            next_right_short_url = response.css("td b a").xpath("@href").extract_first()
+            yield scrapy.Request(response.urljoin(next_right_short_url), callback=self.next_page_parse)
+
+        elif self.config.get(self.name, "last_id") != last_id:
+
+            if "phish_detail.php?phish_id="+self.config.get(self.name, "last_id") in short_urls:
+                last_index = short_urls.index("phish_detail.php?phish_id="+ self.config.get(self.name, "last_id"))
+                self.config.set(self.name, "last_id", last_id)
+                self.config.write(open("scrapy.cfg", "w+"))
+            else:
+                last_index = len(short_urls)
+                if len(response.css("td b a").xpath("@href").extract()) > 1:
+                    next_right_short_url = response.css("td b a").xpath("@href").extract()[1]
+                    yield scrapy.Request(response.urljoin(next_right_short_url), callback=self.parse)
+                else:
+                    next_short_url = response.css("td b a").xpath("@href").extract_first()
+                    yield scrapy.Request(response.urljoin(next_short_url), callback=self.parse)
+
+            for short_url in short_urls[:last_index]:
+                if "phish_id" in short_url:
+                    yield scrapy.Request(response.urljoin(short_url), callback=self.detailed_parse)
 
     def detailed_parse(self, response):
         print "================>[crawl] {}<===============".format(response.url)
@@ -38,3 +65,13 @@ class PhishtankSpider(scrapy.Spider):
         phishtank["submit_time"] = submit_time
 
         yield phishtank
+
+    def next_page_parse(self, response):
+        short_urls = response.css("tr td a").xpath("@href").extract()
+
+        for short_url in short_urls:
+            if "phish_id" in short_url:
+                yield scrapy.Request(response.urljoin(short_url), callback=self.detailed_parse)
+
+        next_right_short_url = response.css("td b a").xpath("@href").extract()[1]
+        yield scrapy.Request(response.urljoin(next_right_short_url), callback=self.next_page_parse)
