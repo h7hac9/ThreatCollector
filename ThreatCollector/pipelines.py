d7d9aef7eeb7b47b4f4a6eae41c1c028c9378125
DaemonShao <daemonshao@gmail.com>
2019-01-09 17:17:07 +0800

    repair piplines

--- ThreatCollector/pipelines.py
+++ ThreatCollector/pipelines.py
@@ -47,7 +47,7 @@ class MongoDBPipeline(object):
         if spider.name == "ransomwaretracker":
             table.ensure_index("host")
 
-        if spider.namne == "torips":
+        if spider.name == "torips":
             table.ensure_index("ip")
 
         table.create_index("add_time", expireAfterSeconds=int(config.get(spider.name, "expire_time")))
